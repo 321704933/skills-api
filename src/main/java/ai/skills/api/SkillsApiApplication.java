0eff352a8e2c29a2429778275b4623b3b9e6914e
YuanXu <321704933@qq.com>
2026-03-12 12:43:16 +0800

    🚀 Chore|工程依赖/工具 引入 MySQL 与本地开发配置

--- src/main/java/ai/skills/api/SkillsApiApplication.java
+++ src/main/java/ai/skills/api/SkillsApiApplication.java
@@ -1,5 +1,7 @@
 package ai.skills.api;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
@@ -11,6 +13,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
  */
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@MapperScan(basePackages = "ai.skills.api", annotationClass = Mapper.class)
 public class SkillsApiApplication {
 
     /**
